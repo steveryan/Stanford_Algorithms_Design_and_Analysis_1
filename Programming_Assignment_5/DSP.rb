221edd09186a5ac30465cd38de7f58caf47cb499
steveryan <steveryan6989@gmail.com>
2020-02-01 12:16:19 -0500

    Programming Assignment 5 complete

--- Programming_Assignment_5/DSP.rb
+++ Programming_Assignment_5/DSP.rb
@@ -23,9 +23,8 @@ raw_array.each do |row|
       $length[row[0].to_i] = {}
     end
     if $length[row[0].to_i][vertex]
-      $length[row[0].to_i][vertex] << length
     else
-      $length[row[0].to_i][vertex] = [length]
+      $length[row[0].to_i][vertex] = length
     end
   end
 end
@@ -41,10 +40,33 @@ $PSP.delete(1)
 $SP[1] = 0
 
 def calculate_PSP(vertex)
-  $graph[vertex].each do |node|
-    if $unexplored.includes(node)
-      
-
+  if $graph[vertex] != nil
+    $graph[vertex].each do |node|
+      if $unexplored.include?(node)
+        psp = $SP[vertex] + $length[vertex][node]
+        $PSP[node] = psp if psp < $PSP[node]
+      end
     end
   end
 end
+$last_node_explored = 1
+
+while !$unexplored.empty? do
+  calculate_PSP($last_node_explored)
+  $last_node_explored = $PSP.sort_by {|k, v| v}.first[0]
+  $unexplored.delete($last_node_explored)
+  $explored << $last_node_explored
+  $SP[$last_node_explored] = $PSP[$last_node_explored]
+  $PSP.delete($last_node_explored)
+end
+puts $SP
+puts "SP to 7 is #{$SP[7]}"
+puts "SP to 37 is #{$SP[37]}"
+puts "SP to 59 is #{$SP[59]}"
+puts "SP to 82 is #{$SP[82]}"
+puts "SP to 99 is #{$SP[99]}"
+puts "SP to 115 is #{$SP[115]}"
+puts "SP to 133 is #{$SP[133]}"
+puts "SP to 165 is #{$SP[165]}"
+puts "SP to 188 is #{$SP[188]}"
+puts "SP to 197 is #{$SP[197]}"
